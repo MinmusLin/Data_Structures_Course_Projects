8dc5ade23452ff64d65a81da062c63a728744683
MinmusLin <minmuslin@outlook.com>
2023-11-30 01:15:45 +0800

    2023/11/30 Updated

--- Power_Grid_Construction_Cost_Simulation_System/power_grid_construction_cost_simulation_system.cpp
+++ Power_Grid_Construction_Cost_Simulation_System/power_grid_construction_cost_simulation_system.cpp
@@ -3,5 +3,27 @@
  * File Name:     power_grid_construction_cost_simulation_system.cpp
  * File Function: 电网建设造价模拟系统的实现
  * Author:        Jishen Lin (林继申)
- * Update Date:   2023/10/20
+ * Update Date:   2023/11/30
  ****************************************************************/
+
+#include <iostream>
+
+/* Macro definition */
+#define MEMORY_ALLOCATION_ERROR -1
+
+/*
+ * Function Name:    main
+ * Function:         Main function
+ * Return Value:     0
+ */
+int main()
+{
+    /* System entry prompt */
+    std::cout << "+--------------------------------------------------+" << std::endl;
+    std::cout << "|               电网建设造价模拟系统               |" << std::endl;
+    std::cout << "|  Power Grid Construction Cost Simulation System  |" << std::endl;
+    std::cout << "+--------------------------------------------------+" << std::endl;
+
+    /* Program ends */
+    return 0;
+}
